c250de9fb1a250b6a7804a9eab5658a8122c25fe
Abhishek <you@example.com>
2022-10-07 14:40:16 +0530

    Added:[Abhishek] UC2 GET Request Method and pass name as

--- HelloMessagingRESTApi/src/main/java/com/bridgelabz/hellomessagingrestapi/controllers/MessageController.java
+++ HelloMessagingRESTApi/src/main/java/com/bridgelabz/hellomessagingrestapi/controllers/MessageController.java
@@ -2,12 +2,21 @@ package com.bridgelabz.hellomessagingrestapi.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+
     @GetMapping("/home")
     public String home(){
         return "Hello from BridgeLabz";
     }
+
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello "+name+" From BridgeLabz";
+    }
+    //localhost:8083/query?name=Mark
+
 }
